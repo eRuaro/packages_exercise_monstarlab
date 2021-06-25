b393066108f9aa3735d742ee0b52569bb9231547
eRuaro <erickson_ruaroii@dlsu.edu.ph>
2021-06-26 07:45:48 +0800

    added assets, updated website model

--- lib/models/website.dart
+++ lib/models/website.dart
@@ -1,6 +1,7 @@
 class Website {
   final String url;
   final String websiteName;
+  final String imageFile;
 
-  const Website({required this.websiteName, required this.url});
+  const Website({required this.websiteName, required this.url, required this.imageFile});
 }

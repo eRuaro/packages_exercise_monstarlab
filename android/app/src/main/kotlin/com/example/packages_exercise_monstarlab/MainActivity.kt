0808e29bd64131b5b6d93f64d9224acd896f69b9
eRuaro <erickson_ruaroii@dlsu.edu.ph>
2021-06-26 07:39:33 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/packages_exercise_monstarlab/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.packages_exercise_monstarlab
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:webviewx/webviewx.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: null,
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  late WebViewXController webController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Favorite Websites',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+      body: ListView(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
